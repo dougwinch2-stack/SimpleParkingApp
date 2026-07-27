c195dabc4d6b5682133934f0fdc34505fa75b551
dougwinch2-stack <dougwinch2@gmail.com>
2026-07-27 12:04:08 +0100

    Add files via upload

---
+++ locations.js
@@ -0,0 +1,158 @@
+// Generated from Hospital Wards.xlsx
+// Edit the spreadsheet and regenerate this file when parking locations change.
+const locationsData = {
+  "1": {
+    "hospital": "Basildon",
+    "name": "RENAL",
+    "latitude": 51.559212,
+    "longitude": 0.452781,
+    "notes": ""
+  },
+  "2": {
+    "hospital": "Basildon",
+    "name": "OUTPATIENTS",
+    "latitude": 51.55818,
+    "longitude": 0.450663,
+    "notes": ""
+  },
+  "3": {
+    "hospital": "Basildon",
+    "name": "JUBILEE ENTRANCE",
+    "latitude": 51.5569224,
+    "longitude": 0.4519968,
+    "notes": ""
+  },
+  "4": {
+    "hospital": "Basildon",
+    "name": "CTC - FRONT",
+    "latitude": 51.557045,
+    "longitude": 0.452337,
+    "notes": ""
+  },
+  "5": {
+    "hospital": "Basildon",
+    "name": "CTC - BACK",
+    "latitude": 51.55659,
+    "longitude": 0.452468,
+    "notes": ""
+  },
+  "6": {
+    "hospital": "Basildon",
+    "name": "MENTAL HEALTH - FRONT",
+    "latitude": 51.557065,
+    "longitude": 0.450527,
+    "notes": ""
+  },
+  "7": {
+    "hospital": "Basildon",
+    "name": "MENTAL HEALTH - BACK",
+    "latitude": 51.556741,
+    "longitude": 0.451309,
+    "notes": ""
+  },
+  "8": {
+    "hospital": "Basildon",
+    "name": "A & E",
+    "latitude": 51.558601,
+    "longitude": 0.451255,
+    "notes": ""
+  },
+  "9": {
+    "hospital": "Basildon",
+    "name": "NURSERY",
+    "latitude": 51.557697,
+    "longitude": 0.455518,
+    "notes": ""
+  },
+  "10": {
+    "hospital": "Basildon",
+    "name": "DAY SURGERY",
+    "latitude": 51.557518,
+    "longitude": 0.452253,
+    "notes": ""
+  },
+  "11": {
+    "hospital": "Basildon",
+    "name": "ENDOSCOPY",
+    "latitude": 51.558453,
+    "longitude": 0.451058,
+    "notes": ""
+  },
+  "14": {
+    "hospital": "St Luke's - Basildon",
+    "name": "ST LUKE'S BASILDON",
+    "latitude": 51.561464,
+    "longitude": 0.451921,
+    "notes": ""
+  },
+  "15": {
+    "hospital": "St Luke's - Dunton",
+    "name": "ST LUKE'S THURROCK (DUNTON)",
+    "latitude": 51.547655,
+    "longitude": 0.405826,
+    "notes": ""
+  },
+  "16": {
+    "hospital": "Southend",
+    "name": "RENAL",
+    "latitude": 51.554627,
+    "longitude": 0.690533,
+    "notes": ""
+  },
+  "17": {
+    "hospital": "Southend",
+    "name": "PET CT SCANNER",
+    "latitude": 51.554208,
+    "longitude": 0.687061,
+    "notes": ""
+  },
+  "18": {
+    "hospital": "Southend",
+    "name": "CARDIGAN ENTRANCE",
+    "latitude": 51.554172,
+    "longitude": 0.686205,
+    "notes": ""
+  },
+  "19": {
+    "hospital": "Southend",
+    "name": "OLD MAIN ENTRANCE",
+    "latitude": 51.553771,
+    "longitude": 0.68772,
+    "notes": ""
+  },
+  "20": {
+    "hospital": "Southend",
+    "name": "TOWER BLOCK ENTRANCE",
+    "latitude": 51.553622,
+    "longitude": 0.689443,
+    "notes": ""
+  },
+  "21": {
+    "hospital": "Southend",
+    "name": "RADIOTHERAPY",
+    "latitude": 51.554365,
+    "longitude": 0.688712,
+    "notes": ""
+  },
+  "22": {
+    "hospital": "Southend",
+    "name": "EYE UNIT",
+    "latitude": 51.554939,
+    "longitude": 0.688601,
+    "notes": ""
+  },
+  "30": {
+    "hospital": "Broomfield",
+    "name": "E-BLOCK",
+    "latitude": 51.774632,
+    "longitude": 0.467356,
+    "notes": ""
+  },
+  "31": {
+    "hospital": "Broomfield",
+    "name": "MAIN ENTRANCE - OUTPATIENTS",
+    "latitude": 51.773486,
+    "longitude": 0.467352,
+    "notes": ""
+  }
+};
---
+++ wards.js
@@ -0,0 +1,676 @@
+// Generated from Hospital Wards.xlsx
+// Edit the spreadsheet and regenerate this file when wards change.
+const wardsData = [
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Cardiac Catheter Labs",
+    "floor": "B",
+    "locationId": "5",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Cardiac Investigations",
+    "floor": "A",
+    "locationId": "4",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Cardiac Rehab",
+    "floor": "A",
+    "locationId": "4",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Chelmer Ward",
+    "floor": "D",
+    "locationId": "5",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Colne Ward",
+    "floor": "C",
+    "locationId": "5",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - CT Scanner",
+    "floor": "A",
+    "locationId": "4",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - High Dependancy Unit (HDU)",
+    "floor": "C",
+    "locationId": "5",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Intensive Therapy Unit (ITU)",
+    "floor": "C",
+    "locationId": "5",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Outpatients",
+    "floor": "A",
+    "locationId": "4",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Pulmonary Lab",
+    "floor": "A",
+    "locationId": "4",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Roding Ward",
+    "floor": "B",
+    "locationId": "5",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Special Echo",
+    "floor": "B",
+    "locationId": "4",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - Thames Ward Day Unit",
+    "floor": "B",
+    "locationId": "5",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CTC - X-Ray",
+    "floor": "A",
+    "locationId": "4",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Accident & Emergency (A&E)",
+    "floor": "C",
+    "locationId": "8",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "A&E Minors",
+    "floor": "C",
+    "locationId": "8",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Bereavement Office",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Birthing Centre",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Blood Testing",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Breast Care Unit",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Bulphan Ward",
+    "floor": "A",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Cardiac Department",
+    "floor": "B",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Cedar Ward",
+    "floor": "B",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Cellular Pathology",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Childrens Hearing Centre",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Child Development Centre",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Children’s Outpatients",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Colposcopy",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Day Nursery",
+    "floor": "A",
+    "locationId": "9",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Day Unit",
+    "floor": "A",
+    "locationId": "10",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Dermatology Unit",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Discharge Lounge",
+    "floor": "C",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Edith Clavell Ward",
+    "floor": "C",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Elizabeth Fry Ward",
+    "floor": "D",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Elsdon Ward",
+    "floor": "A",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Endoscopy",
+    "floor": "C",
+    "locationId": "11",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "ENT",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Fleming Ward",
+    "floor": "B",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Florence Nightingale Ward",
+    "floor": "B",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Frank Ahrens",
+    "floor": "C",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Gymnasium",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Gynaecology Outpatients",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "High Dependency Unit",
+    "floor": "A",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Intensive Therapy Unit (ITU)",
+    "floor": "A",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Joint Replacement Unit",
+    "floor": "B",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Kingswood Ward",
+    "floor": "A",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Labour Ward",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Linford Ward",
+    "floor": "A",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Lionel Cosin Ward",
+    "floor": "C",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Lister Ward",
+    "floor": "B",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Mary Seacole Ward",
+    "floor": "B",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Mary Wright Unit",
+    "floor": "C",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Maternity Assessment Unit",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Maternity Outpatients",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Maxillo-Facial",
+    "floor": "C",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Medical Admissions Unit (MAU)",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Mike Jackman Nuclear Medicine",
+    "floor": "C",
+    "locationId": "11",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Neo Natal ICU",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Outpatients",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Oral Department",
+    "floor": "C",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Orsett Ward",
+    "floor": "C",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Osler Ward",
+    "floor": "C",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "PALS",
+    "floor": "B",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Pasteur Ward",
+    "floor": "B",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Pathology",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Penguin Ward",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Physiotherapy",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Plaster Room",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Post Natal Ward",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Pre-Op Assessment",
+    "floor": "C",
+    "locationId": "2",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Puffin Ward",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Renal Unit",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Restaurant",
+    "floor": "C",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Surgical Referrals Unit (SRU)",
+    "floor": "B",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Stanford Ward",
+    "floor": "A",
+    "locationId": "0",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Ultrasound",
+    "floor": "C",
+    "locationId": "11",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Ultrasound (Maternity)",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Wagtail Ward",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "William Harvey Ward (Rheumatology Day Unit)",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Willow Ward",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "X-Ray",
+    "floor": "C",
+    "locationId": "11",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Katherine Monk Ward",
+    "floor": "A",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Horndon Ward",
+    "floor": "A",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Kate Evelyn Luard Ward",
+    "floor": "A",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Short Stay Ward",
+    "floor": "A",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Laindon Ward",
+    "floor": "A",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Polly's Pad",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Frederick Banting Ward",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Burstead Ward",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Critical Care Unit",
+    "floor": "A",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Marjory Warren Ward",
+    "floor": "B",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Oxygen Clinic Pain Management",
+    "floor": "B",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "CT/MRI Courtyard",
+    "floor": "B",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "James Mackenzie Ward",
+    "floor": "B",
+    "locationId": "3",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Mulberry Assessment Unit",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  },
+  {
+    "hospital": "Basildon",
+    "ward": "Maternity Ultrasound",
+    "floor": "D",
+    "locationId": "1",
+    "notes": ""
+  }
+];
