1caf81c1e01a5fcdf2ca102ab2957e82919d40eb
dougwinch2-stack <dougwinch2@gmail.com>
2026-07-27 16:46:38 +0100

    Add files via upload

--- locations.js
+++ locations.js
@@ -1,158 +1,292 @@
-// Generated from Hospital Wards.xlsx
-// Edit the spreadsheet and regenerate this file when parking locations change.
+// Generated directly from Hospital Wards_10.xlsx
+// Hospital maps and parking-location data.
+const hospitalMaps = {
+  "Basildon": [
+    {
+      "name": "Hospital Map",
+      "file": "map-basildon.jpg"
+    }
+  ],
+  "Broomfield": [
+    {
+      "name": "Hospital Map",
+      "file": "map-broomfield.jpg"
+    }
+  ],
+  "Southend": [
+    {
+      "name": "Hospital Map",
+      "file": "map-southend.jpg"
+    },
+    {
+      "name": "Ground Floor",
+      "file": "map-southend-ground-floor.jpg"
+    },
+    {
+      "name": "Floor 1",
+      "file": "map-southend-floor1.jpg"
+    },
+    {
+      "name": "Floor 2",
+      "file": "map-southend-floor2.jpg"
+    }
+  ]
+};
+
 const locationsData = {
+  "0": {
+    "hospital": "TBC",
+    "name": "TBC",
+    "link": "TBC",
+    "coordinates": "TBC",
+    "driverNotes": ""
+  },
   "1": {
     "hospital": "Basildon",
     "name": "RENAL",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.559212,0.452781",
+    "coordinates": "51.559212,0.452781",
+    "driverNotes": "",
     "latitude": 51.559212,
-    "longitude": 0.452781,
-    "notes": ""
+    "longitude": 0.452781
   },
   "2": {
     "hospital": "Basildon",
     "name": "OUTPATIENTS",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.558180,0.450663",
+    "coordinates": "51.558180,0.450663",
+    "driverNotes": "",
     "latitude": 51.55818,
-    "longitude": 0.450663,
-    "notes": ""
+    "longitude": 0.450663
   },
   "3": {
     "hospital": "Basildon",
     "name": "JUBILEE ENTRANCE",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.5569224,0.4519968",
+    "coordinates": "51.5569224,0.4519968",
+    "driverNotes": "",
     "latitude": 51.5569224,
-    "longitude": 0.4519968,
-    "notes": ""
+    "longitude": 0.4519968
   },
   "4": {
     "hospital": "Basildon",
     "name": "CTC - FRONT",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.557045,0.452337",
+    "coordinates": "51.557045,0.452337",
+    "driverNotes": "",
     "latitude": 51.557045,
-    "longitude": 0.452337,
-    "notes": ""
+    "longitude": 0.452337
   },
   "5": {
     "hospital": "Basildon",
     "name": "CTC - BACK",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.556590,0.452468",
+    "coordinates": "51.556590,0.452468",
+    "driverNotes": "",
     "latitude": 51.55659,
-    "longitude": 0.452468,
-    "notes": ""
+    "longitude": 0.452468
   },
   "6": {
     "hospital": "Basildon",
     "name": "MENTAL HEALTH - FRONT",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.557065,0.450527",
+    "coordinates": "51.557065,0.450527",
+    "driverNotes": "",
     "latitude": 51.557065,
-    "longitude": 0.450527,
-    "notes": ""
+    "longitude": 0.450527
   },
   "7": {
     "hospital": "Basildon",
     "name": "MENTAL HEALTH - BACK",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.556741,0.451309",
+    "coordinates": "51.556741,0.451309",
+    "driverNotes": "",
     "latitude": 51.556741,
-    "longitude": 0.451309,
-    "notes": ""
+    "longitude": 0.451309
   },
   "8": {
     "hospital": "Basildon",
     "name": "A & E",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.558601,0.451255",
+    "coordinates": "51.558601,0.451255",
+    "driverNotes": "",
     "latitude": 51.558601,
-    "longitude": 0.451255,
-    "notes": ""
+    "longitude": 0.451255
   },
   "9": {
     "hospital": "Basildon",
     "name": "NURSERY",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.557697,0.455518",
+    "coordinates": "51.557697,0.455518",
+    "driverNotes": "",
     "latitude": 51.557697,
-    "longitude": 0.455518,
-    "notes": ""
+    "longitude": 0.455518
   },
   "10": {
     "hospital": "Basildon",
     "name": "DAY SURGERY",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.557518,0.452253",
+    "coordinates": "51.557518,0.452253",
+    "driverNotes": "",
     "latitude": 51.557518,
-    "longitude": 0.452253,
-    "notes": ""
+    "longitude": 0.452253
   },
   "11": {
     "hospital": "Basildon",
     "name": "ENDOSCOPY",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.558453,0.451058",
+    "coordinates": "51.558453,0.451058",
+    "driverNotes": "",
     "latitude": 51.558453,
-    "longitude": 0.451058,
-    "notes": ""
+    "longitude": 0.451058
+  },
+  "12": {
+    "hospital": "TBC",
+    "name": "TBC",
+    "link": "TBC",
+    "coordinates": "TBC",
+    "driverNotes": ""
+  },
+  "13": {
+    "hospital": "TBC",
+    "name": "TBC",
+    "link": "TBC",
+    "coordinates": "TBC",
+    "driverNotes": ""
   },
   "14": {
     "hospital": "St Luke's - Basildon",
     "name": "ST LUKE'S BASILDON",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.561464,0.451921",
+    "coordinates": "51.561464,0.451921",
+    "driverNotes": "",
     "latitude": 51.561464,
-    "longitude": 0.451921,
-    "notes": ""
+    "longitude": 0.451921
   },
   "15": {
     "hospital": "St Luke's - Dunton",
     "name": "ST LUKE'S THURROCK (DUNTON)",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.547655,0.405826",
+    "coordinates": "51.547655,0.405826",
+    "driverNotes": "",
     "latitude": 51.547655,
-    "longitude": 0.405826,
-    "notes": ""
+    "longitude": 0.405826
   },
   "16": {
     "hospital": "Southend",
     "name": "RENAL",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.554627,0.690533",
+    "coordinates": "51.554627,0.690533",
+    "driverNotes": "",
     "latitude": 51.554627,
-    "longitude": 0.690533,
-    "notes": ""
+    "longitude": 0.690533
   },
   "17": {
     "hospital": "Southend",
     "name": "PET CT SCANNER",
-    "latitude": 51.554208,
-    "longitude": 0.687061,
-    "notes": ""
+    "link": "https://www.google.com/maps/search/?api=1&query=51.554301,0.690223",
+    "coordinates": "51.554301,0.690223",
+    "driverNotes": "**WARNING** PET CT Scanner has moved and is believed to be in a mobile unit in the main car park|Exact mobile scanner location is not confirmed|Use the car park entrance from Prittlewell Chase, not Carlingford Drive|Google may suggest a route blocked by a fence",
+    "latitude": 51.554301,
+    "longitude": 0.690223
   },
   "18": {
     "hospital": "Southend",
     "name": "CARDIGAN ENTRANCE",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.554172,0.686205",
+    "coordinates": "51.554172,0.686205",
+    "driverNotes": "",
     "latitude": 51.554172,
-    "longitude": 0.686205,
-    "notes": ""
+    "longitude": 0.686205
   },
   "19": {
     "hospital": "Southend",
-    "name": "OLD MAIN ENTRANCE",
+    "name": "PRITTLEWELL ENTRANCE",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.553771,0.687720",
+    "coordinates": "51.553771,0.687720",
+    "driverNotes": "OLD MAIN ENTRANCE",
     "latitude": 51.553771,
-    "longitude": 0.68772,
-    "notes": ""
+    "longitude": 0.68772
   },
   "20": {
     "hospital": "Southend",
     "name": "TOWER BLOCK ENTRANCE",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.553622,0.689443",
+    "coordinates": "51.553622,0.689443",
+    "driverNotes": "",
     "latitude": 51.553622,
-    "longitude": 0.689443,
-    "notes": ""
+    "longitude": 0.689443
   },
   "21": {
     "hospital": "Southend",
     "name": "RADIOTHERAPY",
-    "latitude": 51.554365,
-    "longitude": 0.688712,
-    "notes": ""
+    "link": "https://www.google.com/maps/search/?api=1&query=51.5543812,0.686461",
+    "coordinates": "51.5543812,0.686461",
+    "driverNotes": "",
+    "latitude": 51.5543812,
+    "longitude": 0.686461
   },
   "22": {
     "hospital": "Southend",
     "name": "EYE UNIT",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.554939,0.688601",
+    "coordinates": "51.554939,0.688601",
+    "driverNotes": "",
     "latitude": 51.554939,
-    "longitude": 0.688601,
-    "notes": ""
+    "longitude": 0.688601
   },
-  "30": {
+  "23": {
     "hospital": "Broomfield",
-    "name": "E-BLOCK",
-    "latitude": 51.774632,
-    "longitude": 0.467356,
-    "notes": ""
-  },
-  "31": {
-    "hospital": "Broomfield",
-    "name": "MAIN ENTRANCE - OUTPATIENTS",
+    "name": "Zone-A - Main Entrance & Outpatients",
+    "link": "",
+    "coordinates": "51.773486,0.467352",
+    "driverNotes": "Good for most wards / rooms in Zone-A - some are closer to Zone-E and these will show that",
     "latitude": 51.773486,
-    "longitude": 0.467352,
-    "notes": ""
+    "longitude": 0.467352
+  },
+  "24": {
+    "hospital": "Broomfield",
+    "name": "Zone-B - Back Door",
+    "link": "",
+    "coordinates": "51.774049, 0.463927",
+    "driverNotes": "Good for most wards / rooms in Zone-B and in Zone-C - some are closer to other entrances and these will show that",
+    "latitude": 51.774049,
+    "longitude": 0.463927
+  },
+  "25": {
+    "hospital": "Broomfield",
+    "name": "Zone-C - Back Door",
+    "link": "",
+    "coordinates": "51.774049, 0.463927",
+    "driverNotes": "Good for most wards / rooms in Zone-C and in Zone-B - some are closer to other entrances and these will show that",
+    "latitude": 51.774049,
+    "longitude": 0.463927
+  },
+  "26": {
+    "hospital": "Broomfield",
+    "name": "Zone-D - East Entrance",
+    "link": "",
+    "coordinates": "51.774632,0.467356",
+    "driverNotes": "Not sure of best parking place for Zone-D so use Zone-E for now as it's not awful",
+    "latitude": 51.774632,
+    "longitude": 0.467356
+  },
+  "27": {
+    "hospital": "Broomfield",
+    "name": "Zone-E - East Entrance",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.774632,0.467356",
+    "coordinates": "51.774632,0.467356",
+    "driverNotes": "Use for Zone-E and currently also for Zone-D where indicated",
+    "latitude": 51.774632,
+    "longitude": 0.467356
+  },
+  "28": {
+    "hospital": "Southend",
+    "name": "HILLBOROUGH ENTRANCE",
+    "link": "https://www.google.com/maps/search/?api=1&query=51.5543307,0.6885691",
+    "coordinates": "51.5543307,0.6885691",
+    "driverNotes": "",
+    "latitude": 51.5543307,
+    "longitude": 0.6885691
   }
 };
